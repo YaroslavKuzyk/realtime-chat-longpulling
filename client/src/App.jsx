f3023048bf386351a8a9ccd96448c79a36a9a3c7
Yaroslav Kuzyk <kuzyk.yaril@gmail.com>
2022-06-25 15:49:03 +0300

    first commit

---
+++ client/src/App.jsx
@@ -0,0 +1,50 @@
+import React, {useEffect, useState} from 'react';
+import './app.css'
+import axios from "axios";
+
+const Longpulling = () => {
+    const [messages, setMessages] = useState([])
+    const [value, setValue] = useState('')
+
+    useEffect(() => {
+        subscribe()
+    }, [])
+
+    const subscribe = async () => {
+        try {
+            const { data } = await axios.get('http://localhost:5000/get-message')
+            setMessages(prev => [data, ...prev])
+            await subscribe()
+        } catch (e) {
+            setTimeout(()=> {
+                subscribe()
+            }, 500)
+        }
+    }
+
+    const sendMessage = async () => {
+        await axios.post('http://localhost:5000/new-message', {
+            message: value,
+            id: Date.now()
+        })
+    }
+    return (
+        <div className='center'>
+            <div>
+                <div className="form">
+                    <input value={value} onChange={(e)=> setValue(e.target.value)} type="text"/>
+                    <button onClick={sendMessage}>Отправить</button>
+                </div>
+                <div className="messages">
+                    {messages.map(mess =>
+                        <div className='message' key={mess.id}>
+                            {mess.message}
+                        </div>
+                    )}
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Longpulling;
